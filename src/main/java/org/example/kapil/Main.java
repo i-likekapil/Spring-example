4d777f6fe90628179fd3f46a6714731068a56c05
КคקเL КคยรЂเК <kapilkaushik9143@gmail.com>
2023-01-20 21:56:44 +0530

    spring getter, setter & constructor

--- src/main/java/org/example/kapil/Main.java
+++ src/main/java/org/example/kapil/Main.java
@@ -8,8 +8,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+
         Alien alien = (Alien) context.getBean("alien");
+        Alien alien1 = (Alien) context.getBean("alien");
         System.out.println("age " +alien.getAge());
         alien.can();
+        alien1.can();
     }
 }
